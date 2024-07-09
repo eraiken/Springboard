a81444653bc5c8da43d39b2b363a6f2965660bde
Emily <emily.aiken@alumni.ou.edu>
2024-07-08 22:27:56 -0400

    SQL Case Study
    Commit before submit

--- Unit 19/Case Study - Tier 1/SQLFiles Tier 1/Part 1 - SQLTasks Tier 1.sql
+++ Unit 19/Case Study - Tier 1/SQLFiles Tier 1/Part 1 - SQLTasks Tier 1.sql
@@ -136,37 +136,3 @@ JOIN Members AS m
 ON sub.memid = m.memid
 WHERE sub.cost > 30
 ORDER BY cost DESC;
-
-/* PART 2: SQLite
-/* We now want you to jump over to a local instance of the database on your machine. 
-
-Copy and paste the LocalSQLConnection.py script into an empty Jupyter notebook, and run it. 
-
-Make sure that the SQLFiles folder containing thes files is in your working directory, and
-that you haven't changed the name of the .db file from 'sqlite\db\pythonsqlite'.
-
-You should see the output from the initial query 'SELECT * FROM FACILITIES'.
-
-Complete the remaining tasks in the Jupyter interface. If you struggle, feel free to go back
-to the PHPMyAdmin interface as and when you need to. 
-
-You'll need to paste your query into value of the 'query1' variable and run the code block again to get an output.
- 
-QUESTIONS:
-/* Q10: Produce a list of facilities with a total revenue less than 1000.
-The output of facility name and total revenue, sorted by revenue. Remember
-that there's a different cost for guests and members! */
-
-
-
-/* Q11: Produce a report of members and who recommended them in alphabetic surname,firstname order */
-
-
-
-/* Q12: Find the facilities with their usage by member, but not guests */
-
-
-
-/* Q13: Find the facilities usage by month, but not guests */
-
-
